3f7c5844f2eebdec071b9a1279c040481b372be0
kobayashiryou <chiri.parihead@gmail.com>
2021-07-20 16:53:16 +0900

    company_target model spec

--- spec/factories/company_targets.rb
+++ spec/factories/company_targets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :company_target do
-    monthly_target { "MyString" }
-    anually_target { "MyString" }
-    company { nil }
+    monthly_target { Faker::Lorem.sentence }
+    anually_target { Faker::Lorem.sentence }
+    company
   end
 end
--- spec/models/company_target_spec.rb
+++ spec/models/company_target_spec.rb
@@ -1,5 +1,29 @@
 require "rails_helper"
 
 RSpec.describe CompanyTarget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系テスト" do
+    subject{ company_target.valid? }
+    let(:company_target){ build(:company_target, monthly_target: monthly_target, anually_target: anually_target) }
+    let(:monthly_target){ Faker::Lorem.sentence }
+    let(:anually_target){ Faker::Lorem.sentence }
+    context "monthly_target,anually_targetが指定されている時" do
+      it "monthly_target,anually_targetが作成される" do
+        expect(subject).to eq true
+      end
+    end
+
+    context "monthly_targetのみが指定されている時" do
+      let(:anually_target){ nil }
+      it "monthly_targetが作成される" do
+        expect(subject).to eq true
+      end
+    end
+
+    context "anually_targetのみが指定されてる時" do
+      let(:monthly_target){ nil }
+      it "anually_targetが作成される" do
+        expect(subject).to eq true
+      end
+    end
+  end
 end
