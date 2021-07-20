4261d2ad4158ab47b05687e565ed6804e7f6fd55
kobayashiryou <chiri.parihead@gmail.com>
2021-07-20 16:58:43 +0900

    todo_target model spec

--- spec/models/todo_target_spec.rb
+++ spec/models/todo_target_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe TodoTarget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation check" do
+    subject{ todo_target.valid? }
+    let(:todo_target){ build(:todo_target, body: body) }
+    let(:body){ Faker::Lorem.sentence }
+    context "bodyが指定されている時" do
+      it "todo_targetは作成される" do
+        expect(subject).to eq true
+      end
+    end
+
+    context "bodyがnilの時" do
+      let(:body){ nil }
+      it "エラーする" do
+        subject
+        expect(todo_target.errors.messages[:body]).to include "can't be blank"
+      end
+    end
+  end
 end
