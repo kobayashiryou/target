bfe1d5bd8bb6ec9c6b250fad5d4fa2be9ee7f152
kobayashiryou <chiri.parihead@gmail.com>
2021-07-21 21:25:53 +0900

    add validates each model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :todos, dependent: :destroy
   belongs_to :department
+  validates :username, uniqueness: { case_sensitive: true }, presence: true
 end
