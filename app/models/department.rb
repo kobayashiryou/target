0e363affd6fcb6497be00e46c4ba66acad62d4f6
kobayashiryou <chiri.parihead@gmail.com>
2021-07-21 14:34:56 +0900

    add relation each model

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,4 +3,7 @@ class Department < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :users
+  has_many :todo_targets, dependent: :destroy
+  belongs_to :company
 end
