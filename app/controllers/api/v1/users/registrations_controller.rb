07e90b7d0618ce91cc583a9ba2556a9618107ce2
kobayashiryou <chiri.parihead@gmail.com>
2021-07-21 21:25:26 +0900

    create users/registrations_controller

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :department_id])
+  end
 end
