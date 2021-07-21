24af608ce9736448e031fed57f2f1f5041c3db37
kobayashiryou <chiri.parihead@gmail.com>
2021-07-21 16:03:22 +0900

    set routes each controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
   devise_for :companies
   devise_for :departments
-  devise_for :users
-  resources :company_targets
-  resources :department_targets
-  resources :todo_targets
-  resources :todos
+  namespace :api do
+    namespace :v1 do
+      devise_for :companies, controllers: {
+        sessions: "api/v1/companies/sessions",
+        registrations: "api/v1/companies/registrations",
+      }
+      devise_for :departments, controllers: {
+        sessions: "api/v1/departments/sessions",
+        registrations: "api/v1/departments/registrations",
+      }
+      devise_for :users,  controllers: {
+        sessions: "api/v1/users/sessions",
+        registrations: "api/v1/users/registrations",
+      }
+      resources :company_targets
+      resources :department_targets
+      resources :todo_targets
+      resources :todos
+    end
+  end
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
