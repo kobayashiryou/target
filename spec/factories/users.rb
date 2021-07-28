d8ce8982ae1c6edac4a7158e27c0670c73cccd46
kobayashiryou <chiri.parihead@gmail.com>
2021-07-28 09:11:54 +0900

    add modelname in factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 6) }
+    username{ Faker::JapaneseMedia::OnePiece.character }
     department
   end
 end
