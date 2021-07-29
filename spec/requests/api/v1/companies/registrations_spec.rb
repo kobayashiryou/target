699835d097a71dbf2d517298ca24a0ed3993deff
kobayashiryou <chiri.parihead@gmail.com>
2021-07-29 20:49:24 +0900

    add companies registrations&sessions request spec

---
+++ spec/requests/api/v1/companies/registrations_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "CompanyRegistrations", type: :request do
+  describe "POST api/v1/companies" do
+    subject{ post(api_v1_company_registration_path, params: params) }
+
+    let(:params){ {api_v1_company: attributes_for(:company) } }
+
+
+    it "新規登録されてリダイレクトする" do
+      expect { subject }.to change { Company.count }.by(1)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to "http://www.example.com/api/v1/company_targets"
+    end
+  end
+end
