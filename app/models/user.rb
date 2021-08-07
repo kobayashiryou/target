d9a3e38edb727b9b77a2bf26874c8f46b0c4da84
kobayashiryou <chiri.parihead@gmail.com>
2021-08-07 19:22:34 +0900

    変数名の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,19 @@ class User < ApplicationRecord
   validates :username, uniqueness: { case_sensitive: true }, presence: true
 
   def self.guest
-    company = Company.find_or_create_by!(email: "guest_company@example.com") do |company|
+    guestcompany = Company.find_or_create_by!(email: "guest_company@example.com") do |company|
       company.password = "guestcompany"
       company.companyname = "株式会社guest"
     end
-    department = Department.find_or_create_by!(email: "guest_department@example.com") do |department|
+    guestdepartment = Department.find_or_create_by!(email: "guest_department@example.com") do |department|
       department.password = "guestdepartment"
       department.departmentname = "株式会社guest:開発"
-      department.company_id = company.id
+      department.company_id = guestcompany.id
     end
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
       user.username = "guest"
-      user.department_id = department.id
+      user.department_id = guestdepartment.id
     end
   end
 end
