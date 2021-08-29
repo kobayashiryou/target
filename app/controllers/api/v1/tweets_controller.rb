acedd91f05324cd047c1aee81951e6578badc9d1
kobayashiryou <chiri.parihead@gmail.com>
2021-08-29 15:55:07 +0900

    execution rubocop -a

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TweetsController < ApplicationController
   before_action :move_to_signed_in
-  before_action :set_tweet, only: %i[show, edit, update, destroy]
+  before_action :set_tweet, only: %i[show edit update destroy]
 
   def index
     if current_user
@@ -31,11 +31,11 @@ class Api::V1::TweetsController < ApplicationController
   end
 
   def update
-    @tweet = current_user.tweets.update(tweet_params)
+    @tweet = current_user.tweets.update!(tweet_params)
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to [:api, :v1, ], notice: "投稿を編集しました" }
+        format.html { redirect_to [:api, :v1], notice: "投稿を編集しました" }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -51,18 +51,17 @@ class Api::V1::TweetsController < ApplicationController
 
   private
 
-  def set_tweet
-    @tweet = current_user.tweets.find(params[:id])
-  end
-
-  def tweet_params
-    params.require(:tweet).permit(:body, :image)
-  end
-
-  def move_to_signed_in
-    unless user_sign_in? || department_signed_in? || company_signed_in?
-      redirect_to api_v1_user_session_url
+    def set_tweet
+      @tweet = current_user.tweets.find(params[:id])
     end
-  end
 
+    def tweet_params
+      params.require(:tweet).permit(:body, :image)
+    end
+
+    def move_to_signed_in
+      unless user_sign_in? || department_signed_in? || company_signed_in?
+        redirect_to api_v1_user_session_url
+      end
+    end
 end
