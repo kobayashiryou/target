f8276875fc72fe241f07a699534074414fcfa206
kobayashiryou <chiri.parihead@gmail.com>
2021-09-05 21:35:49 +0900

    edit tweet_model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  has_many :users, dependent: :destroy, through: :likes
   has_many :likes, dependent: :destroy
+  has_many :users, dependent: :destroy, through: :likes
   validates :body, presence: true
   mount_uploader :image, TweetUploader
 end
