d5bcf60b4b24c2ec3e2699d71b74ee490984a176
kobayashiryou <chiri.parihead@gmail.com>
2021-09-08 21:28:56 +0900

    create tweet_likes_controller

---
+++ app/controllers/api/v1/tweet_likes_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::TweetLikesController < ApplicationController
+
+  def index
+    @tweet_likes = Tweet.includes(:likes).where(likes: {user_id: current_user.id}).order(created_at: "DESC")
+  end
+
+  private
+
+    def move_to_signed_in
+      unless user_signed_in? || department_signed_in? || company_signed_in?
+        redirect_to api_v1_user_session_url
+      end
+    end
+end
